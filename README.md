adb654e93d3dd570a379a3675d0ac9d48450f44b
Mohamed <mohamed.h.abukar@gmail.com>
2021-08-02 15:31:12 -0500

    updated file.

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # note-taker
+This is an application that allows you to write and save notes on a web page. In this assignment i had to create a server and two routes for my apis and my htmls. I also had to create a database. 
+# challenges
+Some of the challenges that I encountered was getting the saved notes to appear back on the webpage. I also had a tough times getting the saved notes to be posted on the database.
+# successes
+I got the application to work.
--- server.js
+++ server.js
@@ -13,32 +13,6 @@ app.use(express.json());
 app.use(express.static("public"));
 
 
-
-app.get('/api/notes', (req, res) => {
-    fs.readFile('./db/db.json', function(err, content) {
-        if (err) throw err;
-        console.log(content);
-        res.send(content);
-    });
-});
-
-
-app.post("/api/notes", (req, res) => {
-    fs.readFile("./db/db.json", (err, content) => {
-        if(err) throw err;
-        let newNote = {
-            title: req.body.title,
-            text: req.body.text
-        }
-        const notes = JSON.parse(content)
-        notes.push(newNote);
-        fs.writeFile("./db/db.json", JSON.stringify(notes, null, 2), (err) => {
-            if (err) throw err;
-            console.log("200")
-            res.send(db);
-          });
-    })
-})
 require("./routes/api-routes")(app);
 require("./routes/html-routes")(app);
 app.listen(port, () => console.log(`App listening on port ${port}`));
